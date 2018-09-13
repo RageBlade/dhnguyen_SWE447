1a9c15404a0c0ae6c00e377353a007cd2b1474fa
RageBlade <35580259+RageBlade@users.noreply.github.com>
2018-09-12 19:17:21 -0700

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -10,14 +10,15 @@ function init() {
         alert("Unable to setup WebGL");
         return;
     }
-
+    cone = new Cone (gl,n)
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
-
+   
     render();
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
