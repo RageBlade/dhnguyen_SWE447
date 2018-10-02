b1799d37b393096bc19e1b0a9fdd24b47411ed48
RageBlade <35580259+RageBlade@users.noreply.github.com>
2018-10-01 19:48:09 -0700

    Add files via upload

---
+++ Assignment-5/SolarSystem.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.5,
+    distance : 5,
+    year : 0.5,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 1.25,
+    distance : 10,
+    year : 0.75,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 15,
+    year : 1,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.5,
+    distance : 3,
+    year : 0.5,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.75,
+    distance : 20,
+    year : 1.5,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 2,
+    distance : 25,
+    year : 2,
+    color : [ 1.0, 0.0, 1.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 1.5,
+    distance : 30,
+    year : 2.5,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 2,
+    distance : 35,
+    year : 3,
+    color : [ 0.0, 0.1, 1.0, 1.0 ]
+  },
+  Neptune : {
+    radius : 1.5,
+    distance : 40,
+    year : 2.75,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.5,
+    distance : 45,
+    year : 3.5,
+    color : [ 0.0, 0.0, 0.0, 0.5 ]
+  }
+};
